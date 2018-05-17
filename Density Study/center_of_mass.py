e0b82cc9d2c2c4ea9ac30048d61729f775bdcd25
nhall28 <nhall28@vt.edu>
2018-05-17 15:52:11 -0400

    Updated spreadsheet and code to add 3D printed samaras

--- Density Study/center_of_mass.py
+++ Density Study/center_of_mass.py
@@ -18,18 +18,13 @@ mpl.rc('font', family='serif', serif='Times New Roman')
 labelfont = {'fontsize':9, 'fontname':'Times New Roman'}
 tickfont = {'fontsize':8, 'fontname':'Times New Roman'}
 
-#def func(xdata,s,m):
-#    return np.array([(1/(s*x*np.sqrt(2*np.pi)))*np.exp((-1/(2*(s**2)))*((np.log(x)-m)**2)) for x in xdata],dtype=np.float64)
- 
-#def func2(xdata,a,b,c, d):
-#    return np.array([a*x**(c-1)*(1-d*np.exp(-b*x**c))*np.exp(-b*x**c) for x in xdata], dtype=np.float64)
 
-def func3(xdata,a,b,c):
-    return np.array([a*np.exp(b*x)*np.exp(-(b*x**2))*x+c*np.exp(-x)*x**2 for x in xdata], dtype=np.float64)
+
 
 df = pd.read_excel('Samara Segment Mass.xlsx', sheet_name='Sheet1')
 data_list = df.values.tolist()
 
+#Initialize variables and arrays
 mass_length = matrix = np.zeros((30,21))
 mass = matrix = np.zeros((30,21))
 length = matrix = np.zeros((30,21))
@@ -40,9 +35,15 @@ total_data = matrix = np.zeros((30,200))
 total_data2 = matrix = np.zeros((30,200))
 total_data3 = matrix = np.zeros((30,200))
 total_data4 = matrix = np.zeros((30,200))
+total_data5 = matrix = np.zeros((2,200))
 area2 = matrix = np.zeros((30,1))
 colors = [mpl.cm.viridis(a) for a in np.linspace(0, 1, 30)]
-
+mass2 = matrix = np.zeros((2,21))
+length2 = matrix = np.zeros((2,21))
+mass_length2 = matrix = np.zeros((2,21))
+center_mass2 = matrix = np.zeros((2,1))
+center_percent2 = matrix = np.zeros((2,1))
+center_percent2 = matrix = np.zeros((2,1))
 
 #Plot mass fraction vs length fraction
 for a in range(0,30):
@@ -56,6 +57,7 @@ for a in range(0,30):
     f = interp1d(np.cumsum(X[~X.mask])/np.sum(X[~X.mask]), (Y[~X.mask])/np.sum(Y[~X.mask]), kind='cubic')
     total_data[a] = f(np.linspace(0,.99,200))
     plt.figure(1)
+    plt.title("Mass Fraction vs Length Fraction for Natural Samaras (fit in red)")
 #    plt.plot(np.linspace(0, 0.99, 200), f(np.linspace(0, 0.99, 200)))
     plt.xlabel('Fraction of length', **labelfont)
     plt.ylabel('Mass fraction, $m$', **labelfont)
@@ -63,10 +65,19 @@ for a in range(0,30):
     plt.yticks([0, 0.25, 0.5, 0.75, 1.0], **tickfont)
     plt.ylim([0, 1])
     plt.plot(np.linspace(0, 0.99, 200), f(np.linspace(0, 0.99, 200)), color=colors[a])
+
 for a, c in enumerate(center_percent):
-        plt.plot([c, c], [0, 1], color=colors[a])
+    plt.figure(2)
+    plt.title("Center of Mass at Length Fraction for Natural Samaras (average in red)")
+    plt.xlabel("Fraction of Length")
+    plt.ylabel("Center of Mass")
+    plt.xlim([0,1])
+    plt.xticks([0, 0.25, 0.5, 0.75, 1.0], **tickfont)
+    plt.yticks([0, 0.25, 0.5, 0.75, 1.0], **tickfont)
+    plt.plot([c, c], [0, 1], color=colors[a])
+    plt.plot([np.mean(center_percent),np.mean(center_percent)],[0,1], color='r')
   
-plt.plot([np.mean(center_percent),np.mean(center_percent)],[0,1], color='r')
+
 
 #Plot mass vs length fraction
 for a in range(0,30):
@@ -79,16 +90,14 @@ for a in range(0,30):
     Y = ma.masked_invalid(mass[a])
     g = interp1d(np.cumsum(X[~X.mask])/np.sum(X[~X.mask]), (Y[~X.mask]), kind='cubic')
     total_data2[a] = g(np.linspace(0,.99,200))
-    plt.figure(2)
+    plt.figure(3)
+    plt.title("Mass vs Length Fraction for Natural Samaras")
     plt.plot(np.linspace(0, 0.99, 200), g(np.linspace(0, 0.99, 200)), color=colors[a])
     plt.xlabel('Fraction of length', **labelfont)
     plt.ylabel('Mass, $mg$', **labelfont)
     plt.xticks([0, 0.25, 0.5, 0.75, 1.0], **tickfont)
 
 
-# Find and plot average center of mass percent
-for a, c in enumerate(center_percent):
-        plt.plot([c, c], [0, 60], color=colors[a])
 #Plot Density vs length fraction
 for a in range(0,30):
     mass[a, 1:] = data_list[a+2][1:21]
@@ -102,7 +111,8 @@ for a in range(0,30):
 #    Z = ma.masked_invalid(area[a])
     h = interp1d(np.cumsum(X[~X.mask])/np.sum(X[~X.mask]), (Y[~X.mask])/(area[a]), kind='cubic')
     total_data3[a] = h(np.linspace(0,.99,200))
-    plt.figure(3)
+    plt.figure(4)
+    plt.title("Area Density vs Length Fraction for Natural Samaras")
     plt.plot(np.linspace(0, 0.99, 200), h(np.linspace(0, 0.99, 200)), color=colors[a])
     plt.xlabel('Fraction of length', **labelfont)
     plt.ylabel('Area Density, $mg/mm^2$', **labelfont)
@@ -121,7 +131,8 @@ for a in range(0,30):
     Y = ma.masked_invalid(mass[a])
     j = interp1d(np.cumsum(X[~X.mask])/np.sum(X[~X.mask]), ((Y[~X.mask])/(X[~X.mask]))*np.sum(X[~X.mask])/np.sum(Y[~X.mask]), kind='cubic')
     total_data4[a] = j(np.linspace(0,.99,200))
-    plt.figure(4)
+    plt.figure(5)
+    plt.title("Linear Density vs Length Fraction for Natural Samaras (fit in red)")
     plt.plot(np.linspace(0, 0.99, 200), j(np.linspace(0, 0.99, 200)), color=colors[a])
     plt.xlabel('Fraction of length', **labelfont)
     plt.ylabel('Linear Density', **labelfont)
@@ -130,7 +141,23 @@ for a in range(0,30):
 
 
 
-
+for a in range(0,2):
+    mass2[a, 1:] = data_list[a+98][1:21]
+    length2[a, 1:] = data_list[a+101][1:21]
+    mass_length2[a] = [b*c for b,c in zip(mass2[a],np.cumsum(length2[a]))]
+    center_mass2[a] = np.nansum(mass_length2[a])/np.nansum(mass2[a])
+    center_percent2[a] = center_mass2[a]/np.nansum(length2[a])
+    X2 = ma.masked_invalid(length2[a])
+    Y2 = ma.masked_invalid(mass2[a])
+    w = interp1d(np.cumsum(X2[~X2.mask])/np.sum(X2[~X2.mask]), ((Y2[~X2.mask])/(X2[~X2.mask]))*np.sum(X2[~X2.mask])/np.sum(Y2[~X2.mask]), kind='cubic')
+    total_data5[a] = w(np.linspace(0,.99,200))
+    plt.figure(6)
+    plt.title("Linear Density vs Length Fraction for 3D Printed PLA Samaras (natural fit in red)")
+    plt.plot(np.linspace(0, 0.99, 200), w(np.linspace(0, 0.99, 200)), color=colors[a])
+    plt.xlabel('Fraction of length', **labelfont)
+    plt.ylabel('Linear Density', **labelfont)
+    plt.xticks([0, 0.25, 0.5, 0.75, 1.0], **tickfont)
+    area2[a] = np.sum((((Y[~X.mask])/(X[~X.mask]))*np.sum(X[~X.mask])/np.sum(Y[~X.mask]))*((X[~X.mask])/np.sum(X[~X.mask])))
         
         
 
@@ -139,7 +166,9 @@ for a in range(0,30):
 #area = np.trapz(total_data[5],dx=.1)
 xdata = np.linspace(0,.99,200)
 mean_data = [np.mean(total_data[:, k]) for k in range(len(xdata))]
+mean_data2 = [np.mean(total_data4[:, k]) for k in range(len(xdata))]
 std_data = [np.std(total_data[:, k]) for k in range(len(xdata))]
+std_data2 = [np.std(total_data4[:, k]) for k in range(len(xdata))]
 #xdata = np.vstack([xdata[1:199:1] for k in range(30)]).ravel()
 xdata = np.vstack([xdata for k in range(30)]).ravel()
 #remove 0 entry from array
@@ -156,7 +185,9 @@ y = np.poly1d(z)
 y2 = np.poly1d(z2)
 plt.figure(1)
 plt.plot(xdata, y(xdata), color='r')
-plt.figure(4)
+plt.figure(5)
+plt.plot(xdata, y2(xdata),color='r')
+plt.figure(6)
 plt.plot(xdata, y2(xdata),color='r')
 #plot mean data
 #plt.plot(np.linspace(0,.99,200), mean_data, color='k')
@@ -172,8 +203,16 @@ plt.plot(xdata, y2(xdata),color='r')
 plt.show()
 #area = np.trapz(total_data[3],dx=.1)
 
-plt.figure(5)
+plt.figure(7)
 ax = plt.gca()
 ax.errorbar(xdata, mean_data, yerr=std_data)
 ax.plot(xdata,y(xdata), color='r')
+plt.title("Standard Deviation for Mass Fraction Fit")
+
+plt.figure(8)
+ax = plt.gca()
+ax.errorbar(xdata, mean_data2, yerr=std_data2)
+ax.plot(xdata,y2(xdata), color='r')
+plt.title("Standard Deviation for Linear Density Fit")
+
 
