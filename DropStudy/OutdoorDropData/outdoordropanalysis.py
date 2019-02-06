9a1046bf839b88200efed11cc2c8edf203b79402
gknave <gknave@vt.edu>
2019-02-06 09:20:09 -0700

    Outdoor Drop Analysis now reads all data correctly. TODO: convert to useable data

--- DropStudy/OutdoorDropData/outdoordropanalysis.py
+++ DropStudy/OutdoorDropData/outdoordropanalysis.py
@@ -2,4 +2,17 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_excel('2017-04 Samara Drop Tests.xlsx', header=1)
-print(data)
+
+samara_list = ['SM', 'NM', 'N']
+prop_list = ['Time', 'Dist', 'Ang']
+for index, row in data.iterrows():
+  print(row)
+  if index == 4:
+    print(row['Time - NM'])
+  for samara_type in samara_list:
+    for prop in prop_list:
+      val = row[prop + ' - ' + samara_type]
+      if val == '-':
+        print('No Data Recorded')
+      elif ~np.isnan(val):
+        print(index, prop, samara_type, val)
