67d1710d2eb8be801781c69de0608b7f5084b953
gknave <gknave@vt.edu>
2019-05-15 08:23:43 -0600

    Updated DataViz.py

--- DropStudy/VideoScripts/DataViz.py
+++ DropStudy/VideoScripts/DataViz.py
@@ -2,40 +2,55 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-real_vel = pd.read_csv('RealSamaraData.csv',names=['trial','velocity'])
-norway_vel = pd.read_csv('NorwayMapleData.csv',names=['trial','velocity'])
-silver_vel = pd.read_csv('SilverMapleData.csv',names=['trial','velocity'])
+real_vel = pd.read_csv('RealMapleData.csv',header=0,names=['trial','cutoff','velocity','rotation'])
+norway_vel = pd.read_csv('NorwayMapleData.csv',header=0,names=['trial','cutoff','velocity','rotation'])
+silver_vel = pd.read_csv('SilverMapleData.csv',header=0,names=['trial','cutoff','velocity','rotation'])
 
-vals =list(real_vel['velocity'].values)
+vels =list(real_vel['velocity'].values)
+angs =list(real_vel['rotation'].values)
 Rvelocities  = []; 
-for val in vals:
+Rrotation  = []; 
+for val in zip(vels,angs):
     if not ' ' in val:
-        Rvelocities.append(float(val))
+        Rvelocities.append(float(val[0]))
+        Rrotation.append(float(val[1]))
 
 print('Real Norway maples \nMean: ',np.mean(Rvelocities),'\n','Std: ',np.std(Rvelocities),'\n\n')
 
 plt.figure()
 plt.hist(Rvelocities)
+plt.figure()
+plt.hist(Rrotation)
 
-vals = list(norway_vel['velocity'].values)
+vels = list(norway_vel['velocity'].values)
+angs =list(norway_vel['rotation'].values)
 Nvelocities = []; 
-for val in vals:
+Nrotation  = []; 
+for val in zip(vels,angs):
     if not ' ' in val:
-        Nvelocities.append(float(val))
+        Nvelocities.append(float(val[0]))
+        Nrotation.append(float(val[1]))
 
 print('3D-printed Norway maples \nMean: ',np.mean(Nvelocities),'\n','Std: ',np.std(Nvelocities),'\n\n')
 plt.figure()
 plt.hist(Nvelocities)
+plt.figure()
+plt.hist(Nrotation)
 
-vals =list(silver_vel['velocity'].values)
+vels =list(silver_vel['velocity'].values)
+angs =list(silver_vel['rotation'].values)
 Svelocities  = []; 
-for val in vals:
+Srotation = []; 
+for val in zip(vels,angs):
     if not ' ' in val:
-        Svelocities.append(float(val))
+        Svelocities.append(float(val[0]))
+        Srotation.append(float(val[1]))
 
 print('3D-printed Silver maples \nMean: ',np.mean(Svelocities),'\n','Std: ',np.std(Svelocities),'\n\n')
 
 plt.figure()
 plt.hist(Svelocities)
-
+plt.figure()
+plt.hist(Srotation)
+plt.show()
 
